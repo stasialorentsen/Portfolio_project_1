76621241fa55c86b5e3aef99bbded2e8248d05c5
stasialorentsen <stasia.lorentsen@gmail.com>
2024-03-03 19:58:08 +0100

    unit test update

--- Test/BachelorProgramTest.java
+++ Test/BachelorProgramTest.java
@@ -46,10 +46,31 @@ public class BachelorProgramTest {
     }
 
     @Test
+    public void shouldNotBeValidIfDoesNotHaveEnoughBasicProjects() {
+        bachelorProgram = new BachelorProgram();
+        // Basic courses
+        for (int i = 1; i <= 8; i++) {
+            bachelorProgram.addActivity(new Course(ActivityType.BASIC_COURSE, 5, "Basic course " + i));
+        }
+        // Module courses
+        for (int i = 1; i <= 10; i++) {
+            bachelorProgram.addActivity(new Course(ActivityType.MODULE_COURSE, 5, "Module course " + i));
+        }
+        // Module projects
+        for (int i = 1; i <= 2; i++) {
+            bachelorProgram.addActivity(new Project(ActivityType.MODULE_PROJECT, "Module project " + i));
+        }
+        // Bachelor project
+        bachelorProgram.addActivity(new Project(ActivityType.BACHELOR_PROJECT, "Bachelor project"));
+        //Basic projects
+        bachelorProgram.addActivity(new Course(ActivityType.BASIC_PROJECT, 15,"First basic project"));
+        bachelorProgram.addActivity(new Course(ActivityType.BASIC_PROJECT, 15, "Second basic project"));
+        assertTrue(bachelorProgram.isValid());
+    }
+
+    @Test
     public void testTotalEcts() {
         BachelorProgram bachelorProgram = new BachelorProgram();
-
-        // Create instances of the concrete class
         bachelorProgram.addActivity(new Project(ActivityType.BASIC_PROJECT, "Basisprojekt 1"));
         bachelorProgram.addActivity(new Project(ActivityType.BASIC_PROJECT, "Basisprojekt 2"));
         bachelorProgram.addActivity(new Course(ActivityType.BASIC_COURSE, 5, "Empiriske data"));
